f04e364a252514f2ad97205b8d0dea0226e3cc32
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-15 15:26:37 +0100

    Serialization error printer

--- theories/CeresExtra.v
+++ theories/CeresExtra.v
@@ -1,4 +1,8 @@
+From Coq Require Import String.
+From Coq Require Import List.
 From Ceres Require Import Ceres.
+From Ceres Require CeresParserUtils.
+From Ceres Require CeresString.
 
 
 Local Notation "p >>= f" := (Deser.bind_field p f) (at level 50, left associativity) : deser_scope.
@@ -58,3 +62,29 @@ Definition con10_ {A B C D E F G H I J R} (f : A -> B -> C -> D -> E -> F -> G -
     `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E} `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
   : FromSexpList R :=
   con10 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+
+
+Definition string_of_loc (l : loc) : string := CeresString.comma_sep (map CeresString.string_of_nat l).
+
+Fixpoint string_of_message (print_sexp : bool) (m : message) : string :=
+  match m with
+  | MsgStr s => s
+  | MsgSexp e => if print_sexp then string_of_sexp e else ""
+  | MsgApp m1 m2 =>
+    let m1_str := string_of_message print_sexp m1 in
+    let m2_str := string_of_message print_sexp m2 in
+    m1_str ++ m2_str
+  end.
+
+Definition string_of_error (print_loc print_sexp : bool) (e : error) : string :=
+  match e with
+  (* Errors from parsing [string -> sexp] *)
+  | ParseError e => CeresParserUtils.pretty_error e
+  (* Errors from deserializing [sexp -> A] *)
+  | DeserError l m =>
+    let msg_str := string_of_message print_sexp m in
+    if print_loc
+    then msg_str ++ " at location " ++ string_of_loc l
+    else msg_str
+  end.

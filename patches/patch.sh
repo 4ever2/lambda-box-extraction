64d097459fc8d67037a73f1aed4f1502485b669d
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-28 14:25:02 +0100

    Remove int patch

--- patches/patch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-patch -p0 -N --silent --dry-run < patches/int.patch 2>/dev/null
-if [ $? -eq 0 ]; then
-  patch -p0 < patches/int.patch
-fi
--- theories/Extraction.v
+++ theories/Extraction.v
@@ -14,10 +14,6 @@ From Coq Require Import Extraction.
 
 Extract Inlined Constant Coqlib.proj_sumbool => "(fun x -> x)".
 
-Extract Inductive Decimal.int => "decimal_int" [ "DecimalPos" "DecimalNeg" ] "(fun hp hn d -> match d with DecimalPos p -> hp p | DecimalNeg p -> hn p)".
-Extract Inductive Hexadecimal.int => "hexadecimal_int" [ "HexadecimalPos" "HexadecimalNeg" ] "(fun hp hn d -> match d with HexadecimalPos p -> hp p | HexadecimalNeg p -> hn p)".
-Extract Inductive Number.int => "number_int" [ "IntDecimal" "IntHexadecimal" ] "(fun hp hn d -> match d with IntDecimal p -> hp p | IntHexadecimal p -> hn p)".
-
 Extraction Inline Equations.Prop.Classes.noConfusion.
 Extraction Inline Equations.Prop.Logic.eq_elim.
 Extraction Inline Equations.Prop.Logic.eq_elim_r.

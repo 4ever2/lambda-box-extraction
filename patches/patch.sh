599c51e9da9b8582eb1cc79569f4bb7efca5b202
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-28 14:12:42 +0100

    Clean up patches

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-patch -p1 -N --silent --dry-run < patches/int.patch 2>/dev/null
+patch -p0 -N --silent --dry-run < patches/int.patch 2>/dev/null
 if [ $? -eq 0 ]; then
-  patch -p1 < patches/int.patch
+  patch -p0 < patches/int.patch
 fi

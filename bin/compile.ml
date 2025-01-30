2d41db4eed0e01a60befca094a5b7756804563cc
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-30 15:09:09 +0100

    Escape haskell unicode repr

--- bin/compile.ml
+++ bin/compile.ml
@@ -10,9 +10,43 @@ type verbose = Normal | Quiet | Verbose
 type copts = {verbose: verbose; debug: bool; output_file: string option}
 let mk_copts verbose debug output_file = { verbose; debug; output_file }
 
+let hex_of_int i =
+  i |> int_of_string |> Printf.sprintf "%X"
+
+let utf8encode s =
+    let prefs = [| 0x0; 0xc0; 0xe0 |] in
+    let s1 n = String.make 1 (Char.chr n) in
+    let rec ienc k sofar resid =
+        let bct = if k = 0 then 7 else 6 - k in
+        if resid < 1 lsl bct then
+            (s1 (prefs.(k) + resid)) ^ sofar
+        else
+            ienc (k + 1) (s1 (0x80 + resid mod 64) ^ sofar) (resid / 64)
+    in
+    ienc 0 "" (int_of_string ("0x" ^ s))
+
+let unescape_unicode s =
+  let re = Str.regexp "\\\\[0-9][0-9][0-9]+" in
+  let subst = function
+  | Str.Delim s ->
+    let s = String.sub s 1 (String.length s - 1) in
+    utf8encode (hex_of_int s)
+  | Str.Text s -> s
+  in
+  String.concat "" (List.map subst (Str.full_split re s))
+
+let escape_unicode s =
+  let re = Str.regexp "\\\\[0-9][0-9][0-9]+" in
+  let subst = function
+  | Str.Delim s -> "\\"^s
+  | Str.Text s -> s
+  in
+  String.concat "" (List.map subst (Str.full_split re s))
+
 let read_ast f : program =
   let c = open_in f in
   let s = really_input_string c (in_channel_length c) in
+  let s = escape_unicode s in
   close_in c;
   print_endline "Compiling:";
   print_endline s;
@@ -28,6 +62,7 @@ let read_ast f : program =
 let read_typed_ast f : global_env =
   let c = open_in f in
   let s = really_input_string c (in_channel_length c) in
+  let s = escape_unicode s in
   close_in c;
   print_endline "Compiling:";
   print_endline s;
@@ -57,7 +92,7 @@ let write_wasm_res opts f p =
 
 let write_rust_res opts f p =
   let f = get_out_file opts f "rs" in
-  List.iter (fun s -> output_string f ((Caml_bytestring.caml_string_of_bytestring s) ^ "\n")) p;
+  List.iter (fun s -> output_string f ((unescape_unicode (Caml_bytestring.caml_string_of_bytestring s)) ^ "\n")) p;
   flush f;
   close_out f
 

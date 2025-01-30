3071dd21751f844c474a3e079cecaa6d98eb661c
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-30 13:08:33 +0100

    Implement Rust printing

--- theories/Translations.v
+++ theories/Translations.v
@@ -1,7 +1,11 @@
 From MetaCoq.Erasure Require EAst.
 From LambdaBox Require LambdaBoxToWASM.
+From LambdaBox Require LambdaBoxToRust.
 
 
 
 Definition l_box_to_wasm :=
     LambdaBoxToWASM.run_translation.
+
+Definition l_box_to_rust :=
+    LambdaBoxToRust.box_to_rust.

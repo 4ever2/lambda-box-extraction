4836eca2c00b54f635c2331bddc1bdc0d214cc11
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-02-14 17:34:04 +0100

    Move main serialization definitions to Serialization module

--- theories/Extraction.v
+++ theories/Extraction.v
@@ -3,10 +3,7 @@ From LambdaBox Require CheckWf.
 From LambdaBox Require Eval.
 From LambdaBox Require Translations.
 From LambdaBox Require SerializePrimitives.
-From LambdaBox Require SerializeCommon.
-From LambdaBox Require SerializeEAst.
-From LambdaBox Require SerializeExAst.
-From LambdaBox Require CeresExtra.
+From LambdaBox Require Serialize.
 From Coq Require Import ExtrOcamlBasic.
 From Coq Require Import ExtrOCamlFloats.
 From Coq Require Import ExtrOCamlInt63.
@@ -67,7 +64,7 @@ Separate Extraction Translations.l_box_to_wasm CertiCoqPipeline.show_IR CertiCoq
                     TypedTransforms.mk_params
                     Eval.eval_box Eval.eval_box_typed
                     CheckWf.check_wf_program CheckWf.CheckWfExAst.check_wf_typed_program CheckWf.agda_eflags CheckWf.agda_typed_eflags
-                    SerializeEAst.program_of_string SerializeExAst.global_env_of_string SerializeCommon.kername_of_string CeresExtra.string_of_error
+                    Serialize.program_of_string Serialize.global_env_of_string Serialize.kername_of_string Serialize.string_of_error
                     Floats.Float32.to_bits Floats.Float.to_bits
                     Floats.Float32.of_bits Floats.Float.of_bits
                     Csyntax

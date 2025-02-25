bd4cc798338d4dd166b0db4523a02ddde4899cf3
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-02-25 22:20:52 +0100

    Wellformed checker proofs

--- theories/CheckWf.v
+++ theories/CheckWf.v
@@ -72,8 +72,6 @@ From MetaCoq.Common Require Import Primitive.
 
 
 Section Wf.
-  Context {efl  : EEnvFlags}.
-  Variable Σ : global_context.
 
   Definition assert (b : bool) (s : unit -> string) : (fun T => result T string) unit :=
     if b then Ok tt else Err (s tt).
@@ -105,36 +103,36 @@ Section Wf.
     | primArray => assert has_primarray (fun _ => "Program contains primitive arrays")
     end.
 
-  Fixpoint wellformed (k : nat) (t : term) {struct t} : (fun T => result T string) unit :=
+  Fixpoint wellformed {efl  : EEnvFlags} Σ (k : nat) (t : term) {struct t} : (fun T => result T string) unit :=
     match t with
     | tRel i => assert has_tRel (fun _ => "Program contains tRel") ;; assert (Nat.ltb i k) (fun _ => "Program not closed, invalid tRel " ^ (string_of_nat i))
-    | tEvar ev args => assert has_tEvar (fun _ => "Program contains tEvar") ;; result_forall (wellformed k) args
-    | tLambda _ M => assert has_tLambda (fun _ => "Program contains tLambda") ;; wellformed (S k) M
+    | tEvar ev args => assert has_tEvar (fun _ => "Program contains tEvar") ;; result_forall (wellformed Σ k) args
+    | tLambda _ M => assert has_tLambda (fun _ => "Program contains tLambda") ;; wellformed Σ (S k) M
     | tApp u v =>
       assert has_tApp (fun _ => "Program contains tApp") ;;
-      wellformed k u ;;
-      wellformed k v
+      wellformed Σ k u ;;
+      wellformed Σ k v
     | tLetIn na b b' =>
       assert has_tLetIn (fun _ => "Program contains tLetIn") ;;
-      wellformed k b ;;
-      wellformed (S k) b'
+      wellformed Σ k b ;;
+      wellformed Σ (S k) b'
     | tCase ind c brs =>
       assert has_tCase (fun _ => "Program contains tCase") ;;
-      let brs' := result_forall (fun br => wellformed (#|br.1| + k) br.2) brs in
+      let brs' := result_forall (fun br => wellformed Σ (#|br.1| + k) br.2) brs in
       wf_brs Σ ind.1 #|brs| ;;
-      wellformed k c ;;
+      wellformed Σ k c ;;
       brs'
     | tProj p c =>
       assert has_tProj (fun _ => "Program contains tProj") ;;
       assert_some (lookup_projection Σ p) (fun _ => "Projection " ^ (string_of_inductive p.(proj_ind))^":"^(string_of_nat p.(proj_npars))^","^(string_of_nat p.(proj_arg)) ^ " not found") ;;
-      wellformed k c
+      wellformed Σ k c
     | tFix mfix idx =>
       assert has_tFix (fun _ => "Program contains tFix") ;;
       result_forall (fun t => assert ((isLambda ∘ dbody) t) (fun _ => "Fixpoint body is not a lambda")) mfix ;;
-      wf_fix_gen_ wellformed k mfix idx
+      wf_fix_gen_ (wellformed Σ) k mfix idx
     | tCoFix mfix idx =>
       assert has_tCoFix (fun _ => "Program contains tCoFix") ;;
-      wf_fix_gen_ wellformed k mfix idx
+      wf_fix_gen_ (wellformed Σ) k mfix idx
     | tBox => assert has_tBox (fun _ => "Program contains tBox")
     | tConst kn =>
       assert has_tConst (fun _ => "Program contains tConst") ;;
@@ -149,18 +147,13 @@ Section Wf.
         match lookup_constructor_pars_args Σ ind c with
         | Some (p, a) => assert ((p + a) == #|block_args|) (fun _ => "Constructor " ^ (string_of_inductive ind)^":"^(string_of_nat c) ^ " not fully applied")
         | _ => Ok tt end
-        ;; result_forall (wellformed k) block_args
+        ;; result_forall (wellformed Σ k) block_args
       else assert (is_nil block_args) (fun _ => "Constructor args non-empty")
     | tVar _ => assert has_tVar (fun _ => "Program contains tVar")
-    | tPrim p => has_prim_ p ;; assert (test_prim (fun t => bool_of_result (wellformed k t)) p) (fun _ => "Invalid array primitive")
-    | tLazy t => assert has_tLazy_Force (fun _ => "Program contains lazy/force") ;; wellformed k t
-    | tForce t => assert has_tLazy_Force (fun _ => "Program contains lazy/force") ;; wellformed k t
+    | tPrim p => has_prim_ p ;; assert (test_prim (fun t => bool_of_result (wellformed Σ k t)) p) (fun _ => "Invalid array primitive")
+    | tLazy t => assert has_tLazy_Force (fun _ => "Program contains lazy/force") ;; wellformed Σ k t
+    | tForce t => assert has_tLazy_Force (fun _ => "Program contains lazy/force") ;; wellformed Σ k t
     end.
-
-(*   Theorem wellformed_equiv : forall k t,
-    bool_of_result (wellformed k t) = EWellformed.wellformed Σ k t.
-  Proof.
-  Admitted. *)
 
   Definition wf_projections idecl : (fun T => result T string) unit :=
     match idecl.(ind_projs) with
@@ -175,130 +168,167 @@ Section Wf.
   Definition wf_inductive (idecl : one_inductive_body) : (fun T => result T string) unit :=
     wf_projections idecl.
 
-  Definition wf_minductive (mdecl : mutual_inductive_body) : (fun T => result T string) unit :=
+  Definition wf_minductive {efl  : EEnvFlags} (mdecl : mutual_inductive_body) : (fun T => result T string) unit :=
     assert (has_cstr_params || (mdecl.(ind_npars) == 0)) (fun _ => "") ;;
     result_forall wf_inductive mdecl.(ind_bodies).
 
-  Definition wf_global_decl d : (fun T => result T string) unit :=
+  Definition wf_global_decl {efl  : EEnvFlags} Σ d : (fun T => result T string) unit :=
     match d with
     | ConstantDecl cb =>
       match cb.(cst_body) with
       | None => assert has_axioms (fun _ => "Program contains axioms")
-      | Some cb => wellformed 0 cb
+      | Some cb => wellformed Σ 0 cb
       end
     | InductiveDecl idecl => wf_minductive idecl
     end.
 
+  Fixpoint check_fresh_global (k : kername) (decls : global_declarations) : (fun T => result T string) unit :=
+    match decls with
+    | []    => Ok tt
+    | p::ds => assert (negb (eq_kername (fst p) k)) (fun _ => "Duplicate definition " ^ (string_of_kername (fst p)))  ;; check_fresh_global k ds
+    end.
+
+  Fixpoint check_wf_glob {efl : EEnvFlags} (decls : global_declarations) : (fun T => result T string) unit :=
+    match decls with
+    | []    => Ok tt
+    | p::ds => check_wf_glob ds ;; check_fresh_global (fst p) ;; map_error (fun e => "Error while checking " ^ (string_of_kername (fst p)) ^ ": " ^ e) (wf_global_decl ds (snd p))
+    end.
+
+  Definition check_wf_program {efl : EEnvFlags} (p : program) : (fun T => result T string) unit :=
+    check_wf_glob (fst p) ;; wellformed (fst p) 0 (snd p).
 
 End Wf.
 
-
-
-
-
-Fixpoint check_fresh_global (k : kername) (decls : global_declarations) : (fun T => result T string) unit :=
-  match decls with
-  | []    => Ok tt
-  | p::ds => assert (negb (eq_kername (fst p) k)) (fun _ => "Duplicate definition " ^ (string_of_kername (fst p)))  ;; check_fresh_global k ds
-  end.
-
-Fixpoint check_wf_glob {efl : EEnvFlags} (decls : global_declarations) : (fun T => result T string) unit :=
-  match decls with
-  | []    => Ok tt
-  | p::ds => check_wf_glob ds ;; check_fresh_global (fst p) ;; map_error (fun e => "Error while checking " ^ (string_of_kername (fst p)) ^ ": " ^ e) (wf_global_decl ds (snd p))
-  end.
-
-Definition check_wf_program {efl : EEnvFlags} (p : program) : (fun T => result T string) unit :=
-  check_wf_glob (fst p) ;; wellformed (fst p) 0 (snd p).
-
-(* Definition inspect {A} (a : A) : {b | a = b} := exist _ a eq_refl.
-
-(* freshness boolean check coincides with the freshness property *)
-Fixpoint check_fresh_globalP (k : kername) (decls : global_declarations)
-  : reflectProp (fresh_global k decls) (check_fresh_global k decls).
-Proof.
-  dependent elimination decls; simpl.
-  - apply reflectP.
-    apply Forall_nil.
-  - destruct (inspect (fst p == k)).
-    destruct x; rewrite e; simpl.
-    + apply reflectF => global_ds.
-      dependent elimination global_ds.
-      apply n.
-      by apply eqb_eq.
-    + destruct (check_fresh_globalP k l).
-      * apply reflectP.
-        apply Forall_cons; auto.
-        destruct (neqb (fst p) k) as [Hneq _].
-        apply Hneq.
-        by rewrite e.
-      * apply reflectF => gds.
-        by dependent elimination gds.
-Defined.
-
-(* well-formedness boolean check coincides with the wf property *)
-Fixpoint check_wf_globP {efl : EEnvFlags} (decls : global_declarations)
-  : reflectProp (wf_glob decls) (check_wf_glob decls).
-Proof.
-  dependent elimination decls.
-  - apply reflectP.
-    apply wf_glob_nil.
-  - remember (check_wf_glob l).
-    pose x := (check_wf_globP efl l).
-    rewrite <-Heqb in x.
-    destruct x; simpl; rewrite <-Heqb; simpl.
-    + remember (check_fresh_global (fst p) l).
-      pose x := (check_fresh_globalP (fst p) l).
-      rewrite <-Heqb0 in x.
-      destruct x => /=.
-      remember (wf_global_decl l (snd p)).
-      destruct b.
-      apply reflectP.
-      destruct p.
-      by constructor.
-      apply reflectF => gds.
-      dependent elimination gds.
-      simpl in Heqb1.
-      rewrite <-Heqb1 in i.
-      discriminate i.
-      apply reflectF => gds.
-      by dependent elimination gds.
-    + apply reflectF => gds.
-      by dependent elimination gds.
-Defined. *)
-
-Module CheckWfExAst.
-  Import ExAst.
-
-  Fixpoint check_fresh_global (k : kername) (decls : global_env) : (fun T => result T string) unit  :=
+Section WfCorrect.
+  Fixpoint check_fresh_global_b (k : kername) (decls : global_declarations) : bool :=
     match decls with
-    | []    => Ok tt
-    | (kn,_,_)::ds => assert (negb (eq_kername kn k)) (fun _ => "Duplicate definition " ^ (string_of_kername kn)) ;; check_fresh_global k ds
+    | []    => true
+    | p::ds => negb (eq_kername (fst p) k) && check_fresh_global_b k ds
     end.
 
-  Definition check_wf_typed_program {efl : EEnvFlags} (p : global_env) : (fun T => result T string) unit  :=
-    check_wf_glob (trans_env p).
+  Fixpoint check_wf_glob_b {efl : EEnvFlags} (decls : global_declarations) : bool :=
+    match decls with
+    | []    => true
+    | p::ds => check_wf_glob_b ds && check_fresh_global_b (fst p) ds && EWellformed.wf_global_decl ds (snd p)
+    end.
 
-(*   Fixpoint check_fresh_globalP (k : kername) (decls : global_env)
-    : reflectProp (fresh_global k decls) (check_fresh_global k decls).
+  Definition check_wf_program_b {efl : EEnvFlags} (p : program) : bool :=
+    check_wf_glob_b (fst p) && EWellformed.wellformed (fst p) 0 (snd p).
+
+  Definition inspect {A} (a : A) : {b | a = b} := exist _ a eq_refl.
+
+  (* freshness boolean check coincides with the freshness property *)
+  Fixpoint check_fresh_globalbP (k : kername) (decls : global_declarations)
+    : reflectProp (fresh_global k decls) (check_fresh_global_b k decls).
   Proof.
     dependent elimination decls; simpl.
     - apply reflectP.
       apply Forall_nil.
-    - destruct p as [[kn ?] ?].
-      destruct (inspect (kn == k)).
+    - destruct (inspect (fst p == k)).
       destruct x; rewrite e; simpl.
       + apply reflectF => global_ds.
         dependent elimination global_ds.
-        apply y.
+        apply n.
         by apply eqb_eq.
-      + destruct (check_fresh_globalP k l).
+      + destruct (check_fresh_globalbP k l).
         * apply reflectP.
           apply Forall_cons; auto.
-          destruct (neqb kn k) as [Hneq _].
+          destruct (neqb (fst p) k) as [Hneq _].
           apply Hneq.
           by rewrite e.
         * apply reflectF => gds.
           by dependent elimination gds.
-  Defined. *)
+  Qed.
+
+  (* well-formedness boolean check coincides with the wf property *)
+  Fixpoint check_wf_globbP {efl : EEnvFlags} (decls : global_declarations)
+    : reflectProp (wf_glob decls) (check_wf_glob_b decls).
+  Proof.
+    dependent elimination decls.
+    - apply reflectP.
+      apply wf_glob_nil.
+    - remember (check_wf_glob_b l).
+      pose x := (check_wf_globbP efl l).
+      rewrite <-Heqb in x.
+      destruct x; simpl; rewrite <-Heqb; simpl.
+      + remember (check_fresh_global_b (fst p) l).
+        pose x := (check_fresh_globalbP (fst p) l).
+        rewrite <-Heqb0 in x.
+        destruct x => /=.
+        remember (EWellformed.wf_global_decl l (snd p)).
+        destruct b.
+        apply reflectP.
+        destruct p.
+        by constructor.
+        apply reflectF => gds.
+        dependent elimination gds.
+        simpl in Heqb1.
+        rewrite <-Heqb1 in i.
+        discriminate i.
+        apply reflectF => gds.
+        by dependent elimination gds.
+      + apply reflectF => gds.
+        by dependent elimination gds.
+  Qed.
+
+
+
+  Lemma result_mapb {E1 E2 T : Type} : forall (f : E1 -> E2) (r : result T E1),
+    bool_of_result (map_error f r) = bool_of_result r.
+  Proof.
+    intros.
+    destruct r; reflexivity.
+  Qed.
+
+
+
+  Theorem wellformed_equiv {efl  : EEnvFlags} : forall Σ k t,
+    bool_of_result (wellformed Σ k t) = EWellformed.wellformed Σ k t.
+  Proof.
+  Admitted.
+
+  Theorem wf_global_decl_equiv {efl  : EEnvFlags} : forall Σ d,
+    bool_of_result (wf_global_decl Σ d) = EWellformed.wf_global_decl Σ d.
+  Proof.
+  Admitted.
+
+  Theorem check_fresh_global_equiv : forall k d,
+    bool_of_result (check_fresh_global k d) = check_fresh_global_b k d.
+  Proof.
+  Admitted.
+
+  Theorem check_wf_glob_equiv {efl : EEnvFlags} : forall d,
+    bool_of_result (check_wf_glob d) = check_wf_glob_b d.
+  Proof.
+  Admitted.
+
+  Theorem check_wf_program_equiv {efl : EEnvFlags} : forall p,
+    bool_of_result (check_wf_program p) = check_wf_program_b p.
+  Proof.
+  Admitted.
+
+  Lemma check_fresh_globalP (k : kername) (decls : global_declarations)
+    : reflectProp (fresh_global k decls) (bool_of_result (check_fresh_global k decls)).
+  Proof.
+    rewrite check_fresh_global_equiv.
+    apply check_fresh_globalbP.
+  Qed.
+
+  Lemma check_wf_globP {efl : EEnvFlags} (decls : global_declarations)
+    : reflectProp (wf_glob decls) (bool_of_result (check_wf_glob decls)).
+  Proof.
+    rewrite check_wf_glob_equiv.
+    apply check_wf_globbP.
+  Qed.
+
+End WfCorrect.
+
+
+
+Module CheckWfExAst.
+  Import ExAst.
+
+  Definition check_wf_typed_program {efl : EEnvFlags} (p : global_env) : (fun T => result T string) unit  :=
+    check_wf_glob (trans_env p).
+
 End CheckWfExAst.
